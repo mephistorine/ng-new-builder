e1ef5a6ab927973ef9957011b88e8ab337357802
Sam Bulatov <mephistorine@gmail.com>
2022-08-20 10:26:48 +0300

    Git init

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+import { h } from "preact"
+import { useState } from "preact/hooks"
+
+export function App() {
+  const [ count, setCount ] = useState(0)
+
+  return (
+    <>
+      <div>Hello</div>
+    </>
+  )
+}
